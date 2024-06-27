f4452ab319d1241413b265e11948320372aede57
Dylanson25 <dvillarreal0@ucol.mx>
2024-06-27 09:55:59 -0700

    Pet model datata creation

---
+++ CRUD_HAPPY_PET/Models/IPetRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_HAPPY_PET.Models
+{
+    internal interface IPetRepository
+    {
+        void Add(Pet pet);
+        void Edit(Pet pet);
+        void Delete(int id);
+        IEnumerable<Pet> GoAll();
+        IEnumerable<Pet> GoByValue(); //Search
+    }
+}
