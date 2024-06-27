b43338cd4d167bd4f0cbd1370c0f391a58f8d4d9
Dylanson25 <dvillarreal0@ucol.mx>
2024-06-27 14:57:44 -0700

    Implement the view

--- CRUD_HAPPY_PET/Program.cs
+++ CRUD_HAPPY_PET/Program.cs
@@ -1,9 +1,16 @@
 using CRUD_HAPPY_PET.Views;
-
+using CRUD_HAPPY_PET.Models;
+using CRUD_HAPPY_PET.Presenters;
+using CRUD_HAPPY_PET.Repositories;
+using System.Configuration;
 namespace CRUD_HAPPY_PET
 {
     internal static class Program
     {
+        /*
+         * Jorge told me to you close the door when you leave because 
+         * he is not coming back
+         */
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -13,7 +20,11 @@ namespace CRUD_HAPPY_PET
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new PetView());
+            string sqlConnectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+            IPetView view = new PetView();
+            IPetRepository repository = new PetRepository(sqlConnectionString);
+            new PetPresenter(view, repository);
+            Application.Run((Form)view);
         }
     }
 }
