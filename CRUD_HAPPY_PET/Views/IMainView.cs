8edf7d091e9dfdb2f2dff4ee80b0baa8edbde5df
Dylanson25 <dvillarreal0@ucol.mx>
2024-06-28 07:17:10 -0700

    MainView Interface

---
+++ CRUD_HAPPY_PET/Views/IMainView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_HAPPY_PET.Views
+{
+    internal interface IMainView
+    {
+        event EventHandler ShowPetView;
+        event EventHandler ShowOwnerView;
+        event EventHandler ShowVetsView;
+    }
+}
